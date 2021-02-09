3c4e5f0e8491f470b4df0ebb449b87cc96f02a55
cfor0 <cfor0@outlook.com>
2021-02-09 16:28:00 -0500

    added about me section

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,12 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+function toggleAboutMe() {
+    const x = document.getElementsByClassName("about-container")[0];
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
